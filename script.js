6b484a3afe95fe5a22ef01bcc821dc75f591aa38
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-09-05 01:40:28 +0800

    feat: initial commit for script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Modul für nachhaltige Stadtplanung geladen');
+    // Hier können zukünftige Funktionen hinzugefügt werden
+});
